233242d8fe68c0d1b5eace4b54e212466721f75a
William Lillis <wlillis@umass.edu>
2023-04-17 18:29:38 -0400

    Cleaned up code in flag.c a bit

--- flag.c
+++ flag.c
@@ -11,12 +11,18 @@
 #endif 
 
 #define MAX_PASSCODE_LEN 64
-#define MAX_CMD_BUFF_LEN 512
+#define MAX_CMD_BUFF_LEN 256
 
+// rewrite?
+	// give user access to encrypted flag, and source code used to encrypt the key
+	// leave encrypted key in as a comment, let them reverse engineer it
+
+// fflush(stdout) has to be called with each printf call, otherwise the messages won't send properly over the network connection 
 #define printf_flush(fmt,...) printf(fmt __VA_OPT__(,) __VA_ARGS__); fflush(stdout);
-
-// echo "thisistheflag!" | openssl enc -base64 -e -aes-256-cbc -salt -pass pass:supersecurepasscodenoonewillguessever - pbkdf2 - out flag.enc
-// cat flag.enc | openssl enc -base64 -d -aes-256-cbc -salt -pass pass:supersecurepasscodenoonewillguessever -pbkdf2
+// to encrypt:
+	// echo "thisistheflag!" | openssl enc -base64 -e -aes-256-cbc -salt -pass pass:supersecurepasscodenoonewillguessever -pbkdf2 -out flag.enc
+// to decrypt: 
+	// cat flag.enc | openssl enc -base64 -d -aes-256-cbc -salt -pass pass:supersecurepasscodenoonewillguessever -pbkdf2
 
 // strings binary_file_name | less
 
@@ -35,8 +41,6 @@ int main()
 	fflush(stdin);
 	// prompt and take in user input, sanitize if necessary
 	printf_flush("Hello, user! Enter the passcode in order to decrypt the file! (Less than 64 characters)\n");
-	fflush(stdout);
-	//system("echo Hello, user! Enter the passcode in order to decrypt the file! (Less than 64 characters)\n");
 	char user_in[MAX_PASSCODE_LEN];
 	fgets(user_in, MAX_PASSCODE_LEN, stdin);
 	if ((strlen(user_in) > 0) && (user_in[strlen(user_in) - 1] == '\n')) {
@@ -46,9 +50,9 @@ int main()
 	// prepare the buffer to make a command line call
 	char cmd_buff[MAX_CMD_BUFF_LEN] = "cat flag.enc | openssl enc -base64 -d -aes-256-cbc -salt -pass pass:";
 	// copy the user supplied passcode into the buffer
-	memcpy(cmd_buff + strlen(cmd_buff), user_in, MAX_PASSCODE_LEN);
+	memcpy(cmd_buff + strlen(cmd_buff), user_in, strlen(user_in));
 	// add the last cli argument
-	memcpy(cmd_buff + strlen(cmd_buff), " -pbkdf2\0", strlen(" -pbkdf2\0"));
+	memcpy(cmd_buff + strlen(cmd_buff), " -pbkdf2", strlen(" -pbkdf2"));
 	
 	if (pass_check(user_in)) {
 		printf_flush("Congratulations! Decrypting the file now!\nFlag: ");
